101c85bc8b076769146a545018568f31d46199fa
samatha-07 <146173413+samatha-07@users.noreply.github.com>
2026-02-21 19:58:06 +0530

    train.py

--- src/train.py
+++ src/train.py
@@ -1,23 +1,20 @@
 from src.model import create_baseline_tabnet, create_optimized_tabnet, save_model
 from src.config import MAX_EPOCHS, PATIENCE, BATCH_SIZE
 
-def train_model(X_train, y_train, X_valid, y_valid, model_type="baseline"):
+def train_model(X_train, y_train, X_valid, y_valid, model_type='baseline'):
 
-    if model_type == "baseline":
+    if model_type == 'baseline':
         clf = create_baseline_tabnet()
     else:
         clf = create_optimized_tabnet()
 
     clf.fit(
-        X_train=X_train,
-        y_train=y_train,
+        X_train=X_train, y_train=y_train,
         eval_set=[(X_valid, y_valid)],
-        eval_metric=["accuracy"],
         max_epochs=MAX_EPOCHS,
         patience=PATIENCE,
-        batch_size=BATCH_SIZE,
-        virtual_batch_size=256
+        batch_size=BATCH_SIZE
     )
 
-    save_model(clf, f"tabnet_{model_type}")
+    save_model(clf, f'tabnet_{model_type}')
     return clf
