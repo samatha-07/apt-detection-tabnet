3a6f06f9a45c1708a4db86d0abbfd56707ef016d
samatha-07 <146173413+samatha-07@users.noreply.github.com>
2026-02-21 19:55:56 +0530

    data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,17 +1,13 @@
-import os
 import pandas as pd
+import os
 from src.config import RAW_DATA_DIR
 
 def load_data(file_name="dataset.csv"):
     file_path = os.path.join(RAW_DATA_DIR, file_name)
-
     if not os.path.exists(file_path):
-        raise FileNotFoundError(
-            f"Dataset not found at {file_path}. "
-            "Place dataset inside data/raw/"
-        )
-
-    print(f"[INFO] Loading dataset from {file_path}")
+        raise FileNotFoundError(f"Dataset not found at {file_path}")
+    
+    print(f"[INFO] Loading data from {file_path}...")
     df = pd.read_csv(file_path)
-    print(f"[INFO] Dataset shape: {df.shape}")
+    print(f"[INFO] Data loaded. Shape: {df.shape}")
     return df
